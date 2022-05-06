812a91eae81d5131d218321cf1f5b15f81e7bcce
Facuscar <facundo.scardillo@dracoder.com>
2022-05-06 08:39:46 -0300

    Add: save data in localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,15 @@ export class GifsService {
   private _searchHistory: string[] = [];
   private limit: number = 20;
 
-  public results: any[] = [];
+  public results: Gif[] = [];
 
   get searchHistory(): string[] {
     return [...this._searchHistory];
   }
 
-  constructor( private http: HttpClient ) {}
+  constructor( private http: HttpClient ) {
+    this.retrieveData();
+  }
 
   searchGifs( query: string ): void {
     if(query.trim().length > 0){
@@ -25,16 +28,33 @@ export class GifsService {
       } 
   
       let index = this._searchHistory.findIndex(search => query.toLowerCase() === search.toLowerCase());
+      console.log(index);
   
       if(index  != -1){
-  
-      } else{
-        this._searchHistory.unshift( query );
+        this._searchHistory.splice(index, 1);
       }
+      this._searchHistory.unshift(query); 
     }
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=${this.limit}`).subscribe( (result: any) => {
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=${this.limit}`).subscribe( (result: any) => {
         this.results = result.data;
+        this.saveData();
     });
+   
+  }
+
+  saveData(): void {
+    localStorage.setItem('searchHistory', JSON.stringify(this._searchHistory));
+    localStorage.setItem('results', JSON.stringify(this.results));
+  }
+
+  retrieveData(){
+    if( localStorage.getItem('searchHistory') ) { 
+      this._searchHistory = JSON.parse(localStorage.getItem('searchHistory')!) || [];
+    }
+
+    if( localStorage.getItem('results') ) {
+      this.results = JSON.parse(localStorage.getItem('results')!) || [];
+    }
   }
 }
